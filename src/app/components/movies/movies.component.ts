d5936e5284da2bc9f3e481f57590ba72f2c1c93c
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-07-21 23:31:19 -0500

    end version1

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef,  OnInit, ViewChild} from '@angular/core';
 import { Movie } from 'src/app/interface/movies';
 import { MovieService } from 'src/app/services/movie.service';
+import {debounceTime, distinct, filter, fromEvent, map, Observable, Subscription, switchMap, tap} from "rxjs";
 
 @Component({
   selector: 'app-movies',
@@ -9,17 +10,44 @@ import { MovieService } from 'src/app/services/movie.service';
 })
 export class MoviesComponent implements OnInit {
 
-  movies: Movie[] = []
-  constructor(private moviesService:MovieService) { }
+  movies: Movie[] = [];
+  @ViewChild('movieSearchInput', {static:true}) movieSearchInput!: ElementRef;
+  movies$!: Observable<Movie[]>;
+
+  constructor(private moviesService: MovieService) { }
+
 
   ngOnInit(): void {
+    this.movies$ = fromEvent<Event>(
+      this.movieSearchInput.nativeElement,
+      'keyup'
+    )
+      .pipe(
+        map((event: Event) => {
+          const searchTerm = (event.target as HTMLInputElement).value;
+          return searchTerm;
+        }),
+        filter((searchTerm: string) => searchTerm.length > 3),
+        debounceTime(500),
+        distinct(),
+        switchMap((searchTerm: string) =>
+          this.moviesService.getmovies(searchTerm)
+        )
+        // tap((searchTerm: string) => console.log(searchTerm) )
+      )
+      // .subscribe((movies: Movie[]) => {
+      //   this.movies = movies !== undefined ? movies : [];
+      // });
   }
 
-  getMovies(event: any){
-   const searchTerm = (event.target as HTMLInputElement).value;
-    this.moviesService.getmovies(searchTerm).subscribe(data => {
-      this.movies= data !== undefined ? data : [];
-    })
-  }
+  // getMovies(searchTerm: string){
+  //   this.moviesService.getmovies(searchTerm).subscribe(data => {
+  //     this.movies= data !== undefined ? data : [];
+  //   })
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.movieSubcription.unsubscribe();
+  // }
 
 }
